41ace2a4b86c921519fdbc379739da63739d5e27
Thembisile  Makamu <makamuthe2@gmail.com>
2025-02-19 18:23:47 +0200

    add axios dependency and update ESLint configurations for various packages

--- shopwise/src/chatbot/bot.jsx
+++ shopwise/src/chatbot/bot.jsx
@@ -1,8 +1,29 @@
-import react from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 
-function bot(){
-    <div className='box'>
+function Bot() {
+    const [prompt, setPrompt] = useState("");
+    const [answer, setAnswer] = useState("");
+    const handleButtonClick = () =>{
+        axios.get('https://jsonplaceholder.typicode.com/posts').then(
+            response => {
+                setAnswer(response.data[0].toString());
+            }
+        ).catch(error => {
+            console.error("There was an error!", error);
+        });
+    }
 
-    </div>
+    return (
+        <div className='box'>
+            <span> Enter your shopping list </span>
+            <div className='chat'>
+                <textarea onChange={(event) => setPrompt(event.target.value)} className='text'></textarea>
+                <span>{prompt}</span>
+                <span>{answer}</span>
+                <button onClick={handleButtonClick} className='btn'>Send</button>
+            </div>
+        </div>
+    );
 }
-export default bot;
+export default Bot;
